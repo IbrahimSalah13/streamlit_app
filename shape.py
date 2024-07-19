273ab59fbf98bb6678b584f7971820b706efd798
ibrahim@salah <153787198+salbingo@users.noreply.github.com>
2024-07-19 15:18:11 +0200

    Update shape.py

--- shape.py
+++ shape.py
@@ -24,11 +24,11 @@ if compute_btn:
         time.sleep(2)
         st.write("Area : ", area)
         st.write("Perimeter  :", perimeter)
-x=[i for i in range(100)]
-y = [j for j in range(100, 1000, 10)]
-fig = plt.figure(figsize=(15,10))
-plt.plt(x, y)
-plt.title('figure for testing')
-plt.xlabel('axis x')
-plt.ylabel('axis y')
-st.pyplot(fig)
+        x=[i for i in range(100)]
+        y = [j for j in range(100, 1000, 10)]
+        fig = plt.figure(figsize=(15,10))
+        plt.plt(x, y)
+        plt.title('figure for testing')
+        plt.xlabel('axis x')
+        plt.ylabel('axis y')
+        st.pyplot(fig)
